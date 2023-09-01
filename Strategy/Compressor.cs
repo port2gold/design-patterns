8935f9874658cea8529dc3677e112995e358070f
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-01 21:06:54 +0100

    Design Pattern: Strategy

---
+++ Strategy/Compressor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Strategy
+{
+    internal interface Compressor
+    {
+        void Compress(string filename);
+    }
+}
---
+++ Strategy/ImageStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Strategy
+{
+    internal class ImageStorage
+    {
+        private Compressor compressor;
+        private Filter filter;
+        public ImageStorage(Compressor compressor, Filter filter)
+        {
+            this.compressor = compressor;
+            this.filter = filter;
+        }
+
+        public void StoreImage(string fileName)
+        {
+            compressor.Compress(fileName);
+            filter.Apply(fileName);
+        }
+    }
+}
