178c4a13f947d3f6c5652554c6fb4e886079eecb
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-04 19:58:26 +0100

    clean up

--- Strategy/Compressor.cs
+++ Strategy/Compressor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace design_patterns.Strategy
+﻿namespace design_patterns.Strategy
 {
     internal interface Compressor
     {
