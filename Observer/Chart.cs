ca3e8b2a341a75db7a8a9852bc585d6248ee6961
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-08 18:29:59 +0100

    The push style of communication : Observer Pattern

---
+++ Observer/Chart.cs
@@ -0,0 +1,10 @@
+﻿namespace design_patterns.Observer
+{
+    internal class Chart : Observer
+    {
+        public void Update(int value)
+        {
+            Console.WriteLine($"Chart was updated with value of {value}");
+        }
+    }
+}
---
+++ Observer/SpreadSheet.cs
@@ -0,0 +1,10 @@
+﻿namespace design_patterns.Observer
+{
+    internal class SpreadSheet : Observer
+    {
+        public void Update(int value)
+        {
+            Console.WriteLine($"Spread sheet was updated with value of {value}"); ;
+        }
+    }
+}
