e3996e89af62059547d80fed274e237a715c480a
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-09 20:51:43 +0100

    Mediator Design Pattern

---
+++ Mediator/ArticlesDialogBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Mediator
+{
+    public class ArticlesDialogBox : DialogBox
+    {
+        private ListBox articlesListBox;
+        private TextBox titleTextBox;
+        private Button saveButton;
+
+        public ArticlesDialogBox() : base()
+        {
+            articlesListBox = new ListBox(this);
+            titleTextBox = new TextBox(this);
+            saveButton = new Button(this);
+        }
+
+        public void SimulateUserInteraction()
+        {
+            articlesListBox.SetSelection("Article 1");
+            titleTextBox.SetContent("");
+            titleTextBox.SetContent("Article 2");
+            Console.WriteLine($"TextBox: {titleTextBox.GetContent()}");
+            Console.WriteLine($"Button: {saveButton.IsEnabled()}");
+        }
+
+        public override void Changed(UIControl control)
+        {
+            if(control ==  articlesListBox)
+            {
+                ArticleSelected();
+            }
+            else if(control == titleTextBox)
+            {
+                TitleChanged();
+            }
+        }
+
+        private void TitleChanged()
+        {
+            var content = titleTextBox.GetContent();
+            var isEmpty = string.IsNullOrEmpty(content);
+            saveButton.SetEnabled(!isEmpty);
+        }
+
+        private void ArticleSelected()
+        {
+            titleTextBox.SetContent(articlesListBox.GetSelection());
+            saveButton.SetEnabled(true);
+        }
+    }
+}
---
+++ Mediator/TextBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Mediator
+{
+    internal class TextBox : UIControl
+    {
+        private string content;
+
+        public TextBox(DialogBox owner) : base(owner)
+        {
+        }
+
+        public void SetContent(string content)
+        {
+            this.content = content;
+            owner.Changed(this);
+        }
+
+        public string GetContent()
+        {
+            return this.content;
+        }
+    }
+}
