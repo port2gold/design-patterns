da60f54d1652cea150cd3cac44dec81632fd7633
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-12 16:52:02 +0100

    Clear Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,1 @@
 ﻿
-//Mediator design Pattern
-using design_patterns.Mediator;
-
-var dialog = new ArticlesDialogBox();
-dialog.SimulateUserInteraction();
-
-//Console Result When textbox is not empty and the button knows about it.
-//TextBox: Article 2
-//Button: True
-
-
-//Console Result when text box is empty Button knonws and it is disabled
-//TextBox:
-//Button: False
