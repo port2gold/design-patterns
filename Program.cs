d9f23926aa2f82658b6b75a97c37e6f84305c6b0
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-13 20:38:45 +0100

    clear program

--- Program.cs
+++ Program.cs
@@ -1,23 +1,4 @@
 ﻿
-//Chain Of Responsibility Design Pattern:
-
-// Authenticator   ->     logger  -> Compressor
-
-using design_patterns.ChainOfResponsibility;
-
-
-var compressor = new Compressor(null);
-var logger =  new Logger(compressor);
-var authenticator = new Authenticator(logger);
-var server = new WebServer(authenticator);
-server.Handle(new HttpRequest("admin", "1234"));
-
-
-//Console Result 
-
-//Authentication
-//Logging
-//Compress
 
 
 
