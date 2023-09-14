f1e2f83518b726a4efa61f785f44133463cb4e82
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-14 20:29:25 +0100

    Visitor Pattern

---
+++ Visitor/HighlightOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Visitor
+{
+    public class HighlightOperation : Visitor
+    {
+        public void Visit(AnchorNode anchorNode)
+        {
+            Console.WriteLine("Highligth-Anchor");
+        }
+
+        public void Visit(HeadingNode headingNode)
+        {
+            Console.WriteLine("Highlight-Heading");
+        }
+    }
+}
---
+++ Visitor/HtmlDocument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Visitor
+{
+    public class HtmlDocument
+    {
+        private List<HtmlNode> nodes = new List<HtmlNode>();
+
+        public void Add(HtmlNode node)
+        {
+            nodes.Add(node);
+        }
+
+        public void Execute(Visitor visitor)
+        {
+            foreach (HtmlNode node in nodes)
+            {
+                node.Visit(visitor);
+            }
+        }
+    }
+}
