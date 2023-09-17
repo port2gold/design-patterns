b421f800f4901b7f0cfb4e1dfd248fe4b9fc5e44
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-17 13:12:31 +0100

    Clear program

--- Adapter/CaramelAdapter.cs
+++ Adapter/CaramelAdapter.cs
@@ -1,4 +1,5 @@
-﻿using design_patterns.Adapter.ThirdPartyFilter;
+﻿using design_patterns.Adapter;
+using design_patterns.Adapter.ThirdPartyFilter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,3 +17,27 @@ namespace design_patterns.Adapter
         }
     }
 }
+//Adapter design Pattern
+//Used in situation where interface of a class does not match the interface we expect
+
+
+//using design_patterns.Adapter;
+//using design_patterns.Adapter.ThirdPartyFilter;
+
+//var imageView = new ImageView(new Image());
+//imageView.Apply(new VividFilter());
+
+//Console Result 
+//Applying Vivid Filter
+
+//imageView.Apply(new Caramel() ) This does not match the interface so throws an error
+//imageView.Apply(new CaramelFilter()); // CaramelFilter converts the interface to the expected interface  // Composition is used here
+
+//Console Result 
+//Applying Caramel Filter
+
+
+//imageView.Apply(new CaramelAdapter()); //Inheritance is used for the adapter here but Composition is better
+
+//Console Result 
+//Applying Caramel Filter
