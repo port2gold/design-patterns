b263add9e9ebf52afecc7a0e09358e91b0c01270
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-19 17:55:00 +0100

    Clear program and move files code examples to NotificationService

--- Facade/NotificationService.cs
+++ Facade/NotificationService.cs
@@ -14,7 +14,29 @@ namespace design_patterns.Facade
             var connection = server.Connect("ip");
             var authtoken = server.Authenticate("appid", "key");
             server.Send(authtoken, new Message(message), target);
-            connection.Disconnect()
+            connection.Disconnect();
         }
     }
 }
+
+
+//Facade design pattern 
+//Use to provide simple interface to a complex system.
+
+//it reduces coupling
+
+//using design_patterns.Facade;
+//using System.ComponentModel.DataAnnotations;
+
+//var server = new NotificationServer();
+//var connection = server.Connect("ip");
+//var authToken = server.Authenticate("appId", "key");
+//var message = new Message("Hello World");
+//server.Send(authToken, message, "target");
+
+
+
+
+//Facade implementation wraps and on call is made
+//var notification = new NotificationService();
+//notification.Send("Hello World", "target");
