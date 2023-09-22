87eb7377ba46923a9c327bff84c1961fe5284a98
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-22 18:57:59 +0100

    Singleton Design pattern Used where you want to have a single instance of a class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿//Singleton Design Pattern 
+//Used where you want to have a single instance of a class
+
+using design_patterns.Singleton;
+
+//ConfigManager manager = new ConfigManager();
+//manager.Set("name", "kabir");
+
+//ConfigManager other = new ConfigManager();
+//Console.WriteLine(other.Get("name"));
+
+//Null is returned in this case
+
+
+ConfigManager manager =  ConfigManager.GetInstance();
+manager.Set("name", "kabir");
+
+ConfigManager other = ConfigManager.GetInstance();
+Console.WriteLine(other.Get("name"));
+
+//Console Result 
+
+//kabir
