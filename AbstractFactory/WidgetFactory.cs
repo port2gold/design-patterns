5c9e6a5f12e0dd5da9154319fb4644cb8ce88192
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2023-09-26 20:27:02 +0100

    clear out programs and move abstract factory code examples to AbstractFactory files

--- AbstractFactory/WidgetFactory.cs
+++ AbstractFactory/WidgetFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using design_patterns.AbstractFactory.Ant;
+using design_patterns.AbstractFactory.Material;
+using design_patterns.AbstractFactory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,3 +16,28 @@ namespace design_patterns.AbstractFactory
 
     }
 }
+
+
+////Abstract Factory Design Pattern 
+////Provide an interface for creating families of related objects
+
+//using design_patterns.AbstractFactory;
+//using design_patterns.AbstractFactory.Ant;
+//using design_patterns.AbstractFactory.Material;
+
+//new ContactForm().Render(new MaterialWidgetFactory());
+
+
+////Console Result 
+
+////Material TextBox
+////Material Button
+
+
+
+//new ContactForm().Render(new AntWidgetFactory());
+
+////Console Result
+
+////Ant TextBox
+////Ant Button
